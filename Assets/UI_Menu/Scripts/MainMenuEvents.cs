c4a06b83c2f1252354a962650ed6d64a4acf6e02
ApocBirb <migs1super@gmail.com>
2026-02-17 00:31:09 -0800

    Implemented Exit Button that closes the game

--- Assets/UI_Menu/Scripts/MainMenuEvents.cs
+++ Assets/UI_Menu/Scripts/MainMenuEvents.cs
@@ -1,11 +1,36 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MainMenuEvents : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      private UIDocument _document;
+      private Button _exitButton;
+
+      void OnEnable()
+      {
+          _document = GetComponent<UIDocument>();
+
+        var root = _document.rootVisualElement;
+
+        _exitButton = root.Q<Button>("Exit_Button");
+
+        _exitButton.clicked += OnExitClicked;
+      }
+
+      private void OnExitClicked()
+      {
+          Debug.Log("Exit clicked");
+
+          Application.Quit();
+
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#endif
+      }
+
     }
 
     // Update is called once per frame
