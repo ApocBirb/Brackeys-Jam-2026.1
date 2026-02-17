64b0bd7025b48a5c1e6c8c50b4a0d147602548d9
ApocBirb <migs1super@gmail.com>
2026-02-17 00:54:35 -0800

    Fixed Issue located in recently made main menu script.
    Messed up the code a little bit :(

--- Assets/UI_Menu/Scripts/MainMenuEvents.cs
+++ Assets/UI_Menu/Scripts/MainMenuEvents.cs
@@ -3,39 +3,28 @@ using UnityEngine.UIElements;
 
 public class MainMenuEvents : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-      private UIDocument _document;
-      private Button _exitButton;
+    private UIDocument _document;
+    private Button _exitButton;
 
-      void OnEnable()
-      {
-          _document = GetComponent<UIDocument>();
+    void OnEnable()
+    {
+        _document = GetComponent<UIDocument>();
 
         var root = _document.rootVisualElement;
 
         _exitButton = root.Q<Button>("Exit_Button");
 
         _exitButton.clicked += OnExitClicked;
-      }
-
-      private void OnExitClicked()
-      {
-          Debug.Log("Exit clicked");
-
-          Application.Quit();
-
-#if UNITY_EDITOR
-    UnityEditor.EditorApplication.isPlaying = false;
-#endif
-      }
-
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnExitClicked()
     {
-        
+        Debug.Log("Exit button clicked");
+
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
