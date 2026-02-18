eba6e7350907240a64264e87a5c481ec54e087d0
ApocBirb <migs1super@gmail.com>
2026-02-18 14:59:27 -0800

    Made settings menu branch from main menu and added a volume slider that persists.

--- Assets/UI_Menu/Scripts/MainMenuEvents.cs
+++ Assets/UI_Menu/Scripts/MainMenuEvents.cs
@@ -5,6 +5,11 @@ public class MainMenuEvents : MonoBehaviour
 {
     private UIDocument _document;
     private Button _exitButton;
+    private Button _settingsButton;
+    private Button _settingsBackButton;
+    private Slider _volumeSlider;
+    private VisualElement _mainMenuContainer;
+    private VisualElement _settingsContainer;
 
     void OnEnable()
     {
@@ -13,8 +18,24 @@ public class MainMenuEvents : MonoBehaviour
         var root = _document.rootVisualElement;
 
         _exitButton = root.Q<Button>("Exit_Button");
+        _settingsButton = root.Q<Button>("Settings_Button");
+        _settingsBackButton = root.Q<Button>("Settings_Back_Button");
+        _volumeSlider = root.Q<Slider>("Volume_Slider");
+        _mainMenuContainer = root.Q<VisualElement>("Main_Menu_Container");
+        _settingsContainer = root.Q<VisualElement>("Settings_Container");
+
+        //Persisting Sound Changes start
+        float savedVolume = PlayerPrefs.GetFloat("GameVolume", 1f);
+        AudioListener.volume = savedVolume;
+        _volumeSlider.value = savedVolume;
+
+        _volumeSlider.value = AudioListener.volume;
 
         _exitButton.clicked += OnExitClicked;
+        _settingsButton.clicked += ToggleSettings;
+        _settingsBackButton.clicked += ToggleMainMenu;
+
+        _volumeSlider.RegisterValueChangedCallback(OnVolumeChanged);
     }
 
     private void OnExitClicked()
@@ -26,5 +47,39 @@ public class MainMenuEvents : MonoBehaviour
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
+    }
+
+    private void ToggleSettings()
+    {
+        if (_settingsContainer.style.display == DisplayStyle.None)
+        {
+            _settingsContainer.style.display = DisplayStyle.Flex;
+            _mainMenuContainer.style.display = DisplayStyle.None;
+        }
+        else
+        {
+            _settingsContainer.style.display = DisplayStyle.None;
+        }
+    }
+
+    private void ToggleMainMenu()
+    {
+        if (_mainMenuContainer.style.display == DisplayStyle.None)
+        {
+            _mainMenuContainer.style.display = DisplayStyle.Flex;
+            _settingsContainer.style.display = DisplayStyle.None;
+        }
+        else
+        {
+            _mainMenuContainer.style.display = DisplayStyle.None;
+        }
+    }
+
+    private void OnVolumeChanged(ChangeEvent<float> evt)
+    {
+        AudioListener.volume = evt.newValue;
+        Debug.Log("Volume set to: " + evt.newValue);
+        PlayerPrefs.SetFloat("GameVolume", evt.newValue);
+        PlayerPrefs.Save();
     }
 }
